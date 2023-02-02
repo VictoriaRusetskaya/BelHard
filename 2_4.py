aec4008d1c4c380292b9f894b774504bce4e8b14
Victoria <vikrus95@list.ru>
2023-02-02 17:29:09 +0300

    Changed 2_4

--- 2_4.py
+++ 2_4.py
@@ -1,3 +1,8 @@
-string_numbers = input('Enter three numbers: ')
-negative_count = string_numbers.count('-')
+# string_numbers = input('Enter three numbers: ')
+# negative_count = string_numbers.count('-')
+# print('Number of negative: ', negative_count, '\nNumber of positive: ', (3 - negative_count))
+
+negative_count = int(int(input('Enter first number: ')) < 0) + \
+                 int(int(input('Enter second number: ')) < 0) + \
+                 int(int(input('Enter third number: ')) < 0)
 print('Number of negative: ', negative_count, '\nNumber of positive: ', (3 - negative_count))
