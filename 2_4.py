5d47f7632fb6ef48be8237032edc0e7954e4c23e
Victoria <vikrus95@list.ru>
2023-02-02 17:45:00 +0300

    Changed 2_4

--- 2_4.py
+++ 2_4.py
@@ -2,7 +2,7 @@
 # negative_count = string_numbers.count('-')
 # print('Number of negative: ', negative_count, '\nNumber of positive: ', (3 - negative_count))
 
-negative_count = int(int(input('Enter first number: ')) < 0) + \
-                 int(int(input('Enter second number: ')) < 0) + \
-                 int(int(input('Enter third number: ')) < 0)
-print('Number of negative: ', negative_count, '\nNumber of positive: ', (3 - negative_count))
+negative_count = (int(input('Enter first number: ')) < 0) + \
+                 (int(input('Enter second number: ')) < 0) + \
+                 (int(input('Enter third number: ')) < 0)
+print('Number of negative: ', int(negative_count), '\nNumber of positive: ', (3 - negative_count))
