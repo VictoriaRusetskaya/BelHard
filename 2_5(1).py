beab6ccedb2222a3522ffeeb8074fc5b8a22ffaf
Victoria <vikrus95@list.ru>
2023-02-03 18:59:44 +0300

    Add other option

--- 2_5(1).py
+++ 2_5(1).py
@@ -1,2 +1,10 @@
 text = input('Enter phrase: ')
 print("Word count: ", len(text.split()))
+
+# Second option
+phrase = text.strip()
+print('Word count: ', phrase.count(' ') + 1)
+
+# Third option
+phrase = ' '.join(text.split())
+print('Word count: ', phrase.count(' ') + 1)
