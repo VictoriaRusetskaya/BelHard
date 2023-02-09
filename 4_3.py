045760dd047d3aab5be4075a64f67ec03d3185ea
Victoria <vikrus95@list.ru>
2023-02-09 20:04:38 +0300

    Homework lesson 4, change

--- 4_3.py
+++ 4_3.py
@@ -1,14 +1,23 @@
-try:
-    n = int(input('Enter N: '))
-except ValueError:
-    print('You enter not a number!')
-else:
-    num = 2
-    line = []
-    while num <= n:
-        while len(line) < 5:
-            if not(num % 2):
-                line.append(num)
-            num += 1
-        print(line)
-        line = []
+# try:
+#     n = int(input('Enter N: '))
+# except ValueError:
+#     print('You enter not a number!')
+# else:
+#     num = 2
+#     line = []
+#     while num <= n:
+#         while len(line) < 5:
+#             if not(num % 2):
+#                 line.append(num)
+#             num += 2
+#         print(line)
+#         line = []
+
+n = int(input('Enter N: '))
+count = 0
+for num in range(2, n+1, 2):
+    count += 1
+    if count % 5:
+        print(num, end=' ')
+    else:
+        print(num)
